b7cb9856c8fdec7c326c98ca9fa2129a62c524ea
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2025-03-26 04:27:50 +0800

    feat: update Button component styling; modify hover effects for primary and secondary variants

--- portfolio/src/components/Button.jsx
+++ portfolio/src/components/Button.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Button = ({children, onClick, variant, className}) => {
     const variantClasses = {
-        primary: `bg-blueSecondary text-white px-[49px] py-[7px] rounded-full dm-sans-text-bold hover:bg-transparent hover:text-blueSecondary border border-blueSecondary transition-all ${className}`,
-        secondary: `bg-transparent border border-blueSecondary text-blueSecondary px-[49px] py-[7px] rounded-full dm-sans-text-bold hover:bg-blueSecondary hover:text-white transition-all ${className}`,
+        primary: `bg-blueSecondary text-white px-[49px] py-[7px] rounded-full dm-sans-text-bold hover:bg-[#425A62] border border-white transition-all ${className}`,
+        secondary: `bg-transparent border border-blueSecondary text-blueSecondary px-[49px] py-[7px] rounded-full dm-sans-text-bold hover:bg-[#71A4B64D] hover:opacity-80 transition-all ${className}`,
     }
 
   return (
